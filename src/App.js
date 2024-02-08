e8957d57ffbfcd7090ef4f97542a15f7bbf51372
yashp119 <125194772+yashp119@users.noreply.github.com>
2024-02-08 13:25:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello amplify
+          Hello Bitrix Niharika
         </a>
       </header>
     </div>
